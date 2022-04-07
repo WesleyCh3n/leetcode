720707da7b4f0c76b992fad2d98850a9e7196221
WesleyCh3n <wesley.ch3n.0530@gmail.com>
2022-04-07 18:02:39 +0800

    🍁 insolve

--- cpp/s0046/solution.cc
+++ cpp/s0046/solution.cc
@@ -3,8 +3,7 @@
 
 using namespace std;
 
-template <typename T>
-void print_vec(vector<T> &v) {
+template <typename T> void print_vec(vector<T> &v) {
   cout << '[' << ' ';
   for (auto i : v)
     cout << i << ' ';
@@ -12,13 +11,34 @@ void print_vec(vector<T> &v) {
 }
 
 class Solution {
+private:
+  void solve(vector<vector<int>> &ans, vector<int> comb, vector<int> nums, int start) {
+    if ((int)comb.size() == (int)nums.size()) {
+      ans.push_back(comb);
+      return;
+    }
+    for (int i = start; i < (int)nums.size() - 1; ++i) {
+      swap(nums[i], nums[i+1]);
+      solve(ans, comb, nums, i);
+      swap(nums[i], nums[i+1]);
+    }
+  }
 public:
+  vector<vector<int>> permute(vector<int> &nums) {
+    vector<vector<int>> ans;
+    vector<int> comb;
+    solve(ans, comb, nums, 0);
+    return ans;
+  }
 };
 
 int main() {
-  std::vector<int> v = {};
+  std::vector<int> v = {1, 2, 3};
 
   Solution s;
-
+  auto ans = s.permute(v);
+  for (auto v: ans) {
+    print_vec(v);
+  }
   return 0;
 }
