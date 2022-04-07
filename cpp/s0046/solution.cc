86cfa7ffc7a37f340a1ddd4cee428139cbbff82b
WesleyCh3n <wesley.ch3n.0530@gmail.com>
2022-04-07 17:44:52 +0800

    🍁 clean up makefile

--- cpp/s0046/solution.cc
+++ cpp/s0046/solution.cc
@@ -1,10 +1,10 @@
-#include <algorithm>
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
-template <typename T> void print_vec(vector<T> &v) {
+template <typename T>
+void print_vec(vector<T> &v) {
   cout << '[' << ' ';
   for (auto i : v)
     cout << i << ' ';
@@ -12,36 +12,13 @@ template <typename T> void print_vec(vector<T> &v) {
 }
 
 class Solution {
-private:
-  void solve(vector<vector<int>> &ans, vector<int> comb, int target, int num,
-             int start) {
-    if ((int)comb.size() == num && target == 0) {
-      ans.push_back(comb);
-      return;
-    }
-    if (target < 0)
-      return;
-    for (int i = start; i <= 9; ++i) {
-      comb.push_back(i);
-      solve(ans, comb, target - i, num, i + 1);
-      comb.pop_back();
-    }
-  }
-
 public:
-  vector<vector<int>> combinationSum3(int k, int n) {
-    vector<vector<int>> ans;
-    vector<int> comb;
-    solve(ans, comb, n, k, 1);
-    return ans;
-  }
 };
 
 int main() {
+  std::vector<int> v = {};
+
   Solution s;
-  auto ans = s.combinationSum3(3, 9);
-  for (auto v : ans) {
-    print_vec(v);
-  }
+
   return 0;
 }
