c984aae72419194ac69c99362846203d43305b66
WesleyCh3n <wesley.ch3n.0530@gmail.com>
2022-11-01 15:33:02 +0800

    add macro

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -10,3 +10,76 @@ impl ListNode {
         ListNode { next: None, val }
     }
 }
+
+use std::cell::RefCell;
+use std::rc::Rc;
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+#[macro_export]
+macro_rules! btree {
+    () => {
+        None
+    };
+    ($($e:expr), *) => {
+        {
+
+            let elems = vec![$(stringify!($e)), *];
+            let elems = elems.iter().map(|n| n.parse::<i32>().ok()).collect::<Vec<_>>();
+            let head = Some(Rc::new(RefCell::new($crate::TreeNode::new(elems[0].unwrap()))));
+            let mut nodes = std::collections::VecDeque::new();
+            nodes.push_back(head.as_ref().unwrap().clone());
+
+            for i in elems[1..].chunks(2) {
+                let node = nodes.pop_front().unwrap();
+                if let Some(val) = i[0]{
+                    node.borrow_mut().left = Some(Rc::new(RefCell::new($crate::TreeNode::new(val))));
+                    nodes.push_back(node.borrow().left.as_ref().unwrap().clone());
+                }
+                if i.len() > 1 {
+                    if let Some(val) = i[1] {
+                        node.borrow_mut().right = Some(Rc::new(RefCell::new($crate::TreeNode::new(val))));
+                        nodes.push_back(node.borrow().right.as_ref().unwrap().clone());
+                    }
+                }
+            }
+            head
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! linkedlist {
+    () => {
+        None
+    };
+    ($($e:expr), *) => {
+        {
+            let mut head = Box::new($crate::ListNode::new(0));
+            let mut ref_head = &mut head;
+
+            $(
+            ref_head.next = Some(Box::new($crate::ListNode::new($e)));
+            ref_head = ref_head.next.as_mut().unwrap();
+            )*
+
+            let _ = ref_head;
+            head.next
+        }
+    };
+}
