335d37714baf849623de2f60983547226b940235
WesleyCh3n <wesley.ch3n.0530@gmail.com>
2022-11-23 17:08:47 +0800

    add: problem

--- rust/src/s0039.rs
+++ rust/src/s0039.rs
@@ -1,6 +1,29 @@
 impl Solution {
     pub fn combination_sum(candidates: Vec<i32>, target: i32) -> Vec<Vec<i32>> {
-        todo!()
+        let mut curr = Vec::new();
+        let mut result = Vec::new();
+        Self::backtracking(&candidates, target, &mut result, &mut curr);
+        result
+    }
+    fn backtracking(
+        candidates: &[i32],
+        target: i32,
+        result: &mut Vec<Vec<i32>>,
+        curr: &mut Vec<i32>,
+    ) {
+        let sum = curr.iter().sum::<i32>();
+        if sum == target {
+            result.push(curr.to_owned());
+            return;
+        }
+        if sum > target {
+            return;
+        }
+        for (i, &c) in candidates.iter().enumerate() {
+            curr.push(c);
+            Self::backtracking(&candidates[i..], target, result, curr);
+            curr.pop();
+        }
     }
 }
 
@@ -13,11 +36,9 @@ mod tests {
     #[test]
     fn test() {
         Solution {};
-        pub fn combination_sum(
-            candidates: Vec<i32>,
-            target: i32,
-        ) -> Vec<Vec<i32>> {
-            todo!()
-        }
+        assert_eq!(
+            vec![vec![2, 2, 3], vec![7]],
+            dbg!(Solution::combination_sum(vec![2, 3, 6, 7], 7))
+        );
     }
 }
